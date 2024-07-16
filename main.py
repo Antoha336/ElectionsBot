aeddbbc4dfe6e40910e3e12590dd4732239b97ab
Antes <coderasylum@gmail.com>
2024-07-17 00:02:25 +0300

    feat: add "back" button functionality

--- main.py
+++ main.py
@@ -30,6 +30,15 @@ def send_welcome(message):
                      reply_markup=main_menu)
 
 
+@bot.callback_query_handler(func=lambda call: call.data.startswith('main_menu'))
+def handle(call):
+    bot.edit_message_text(text=main_menu_text,
+                          chat_id=call.message.chat.id,
+                          message_id=call.message.id,
+                          parse_mode='html',
+                          reply_markup=main_menu)
+
+
 @bot.callback_query_handler(func=lambda call: call.data.startswith('poll'))
 def handle(call):
     data = call.data.split()
